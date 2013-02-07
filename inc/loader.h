c8ebcbe953055c2ce47bf66a0954e037cb40e031
Jiannan Ouyang <jiannan.ouyang@gmail.com>
2013-02-06 20:26:27 -0500

    debugging identity mapping page table

--- inc/loader.h
+++ inc/loader.h
@@ -7,9 +7,10 @@ typedef struct bootstrap_pgt {
   pgd_2MB_t level4_pgt[NUM_PGD_ENTRIES];
   pud_2MB_t level3_ident_pgt[NUM_PUD_ENTRIES];
   pmd_2MB_t level2_ident_pgt[NUM_PMD_ENTRIES]; // 512 * 2M = 1G
+  pte_2MB_t level2_ident_pgt[NUM_PMD_ENTRIES]; // 512 * 2M = 1G
   // There is no level 1 pte
 } bootstrap_pgt_t;
 
-void bootstrap_pgtable_init(unsigned long mem_base, unsigned long mem_len);
+void pgtable_setup_ident(unsigned long mem_base, unsigned long mem_len);
 long load_image(char *path, unsigned long mem_base);
 #endif
