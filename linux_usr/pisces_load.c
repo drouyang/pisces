1c5ffe8163dbc5e9e3e7b3ab59a1d9ae73aa4a0d
Jack Lange <jacklange@cs.pitt.edu>
2013-04-30 18:20:57 -0400

    helper utilities

---
+++ linux_usr/pisces_load.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <fcntl.h>
+#include <sys/ioctl.h> 
+
+#include "../src/pisces_dev.h"
+
+int main(int argc, char ** argv) {
+    char * kern = argv[1];
+    char * initrd = argv[2];
+    char * cmd_line = argv[3];
+
+    int pisces_fd = 0;
+    struct pisces_image * img = NULL;
+
+
+    img = malloc(sizeof(struct pisces_image));
+
+
+    strcpy(img->kern_path, kern);
+    strcpy(img->initrd_path, initrd);
+    strcpy(img->cmd_line, cmd_line);
+
+
+
+
+    pisces_fd = open("/dev/" DEVICE_NAME, O_RDONLY);
+
+    if (pisces_fd == -1) {
+        printf("Error opening fastmem control device\n");
+        return -1;
+    }
+
+    ioctl(pisces_fd, P_IOCTL_LOAD_IMAGE, img); 
+    /* Close the file descriptor.  */ 
+    close(pisces_fd);
+    
+
+
+}
---
+++ load.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+
+
+./linux_usr/mem_reserve 1
+./linux_usr/pisces_load /home/jarusl/pisces/kitten/vmlwk.bin /home/jarusl/pisces/kitten/init_task "console=pisces"
