a88d71ee2bbc9f11347784a93c0eedc1cf01c2a9
Jack Lange <jacklange@cs.pitt.edu>
2014-06-06 19:16:27 -0400

    format fixes

--- src/enclave_fs.c
+++ src/enclave_fs.c
@@ -21,19 +21,21 @@ static int file_eq_fn(uintptr_t key1, uintptr_t key2) {
 }
 
 
-int enclave_vfs_open_lcall(struct pisces_enclave * enclave, 
-		     struct pisces_xbuf_desc * xbuf_desc, 
-		     struct vfs_open_lcall * lcall) {
-    struct enclave_fs * fs_state = &(enclave->fs_state);
-    struct pisces_lcall_resp vfs_resp;
-    struct file * file_ptr = NULL;
+int 
+enclave_vfs_open_lcall(struct pisces_enclave   * enclave, 
+		       struct pisces_xbuf_desc * xbuf_desc, 
+		       struct vfs_open_lcall   * lcall) 
+{
+    struct enclave_fs        * fs_state = &(enclave->fs_state);
+    struct file              * file_ptr = NULL;
+    struct pisces_lcall_resp   vfs_resp;
 
     debug("Opening file %s (xbuf_desc=%p)\n", lcall->path, xbuf_desc);
 
     file_ptr = file_open(lcall->path, lcall->mode);
 
     if (IS_ERR(file_ptr)) {
-	vfs_resp.status = 0;
+	vfs_resp.status   = 0;
 	vfs_resp.data_len = 0;
 	pisces_xbuf_complete(xbuf_desc, (u8 *)&vfs_resp, sizeof(struct pisces_lcall_resp));
 	return 0;
@@ -45,7 +47,7 @@ int enclave_vfs_open_lcall(struct pisces_enclave * enclave,
 
     fs_state->num_files++;
 
-    vfs_resp.status = (u64)file_ptr;
+    vfs_resp.status   = (u64)file_ptr;
     vfs_resp.data_len = 0;
 
     debug("Returning from open (file_handle = %p)\n", (void *)file_ptr);
@@ -55,12 +57,14 @@ int enclave_vfs_open_lcall(struct pisces_enclave * enclave,
 }
 
 
-int enclave_vfs_close_lcall(struct pisces_enclave * enclave, 
-			    struct pisces_xbuf_desc * xbuf_desc, 
-			    struct vfs_close_lcall * lcall) {
-    struct enclave_fs * fs_state = &(enclave->fs_state);
-    struct pisces_lcall_resp vfs_resp;
-    struct file * file_ptr = NULL;
+int 
+enclave_vfs_close_lcall(struct pisces_enclave   * enclave, 
+			struct pisces_xbuf_desc * xbuf_desc, 
+			struct vfs_close_lcall  * lcall) 
+{
+    struct enclave_fs        * fs_state = &(enclave->fs_state);
+    struct file              * file_ptr = NULL;
+    struct pisces_lcall_resp   vfs_resp;
 
     file_ptr = (struct file *)lcall->file_handle;
 
@@ -69,11 +73,15 @@ int enclave_vfs_close_lcall(struct pisces_enclave * enclave,
 
     
     if (!htable_search(fs_state->open_files, (uintptr_t)file_ptr)) {
+
 	printk("File %p does not exist\n", file_ptr);
+
 	// File does not exist
-	vfs_resp.status = -1;
-	vfs_resp.data_len = 0;
+	vfs_resp.status   = -1;
+	vfs_resp.data_len =  0;
+
 	pisces_xbuf_complete(xbuf_desc, (u8 *)&vfs_resp, sizeof(struct pisces_lcall_resp));
+
 	return 0;
     }
 
@@ -82,7 +90,7 @@ int enclave_vfs_close_lcall(struct pisces_enclave * enclave,
 
     file_close(file_ptr);
 
-    vfs_resp.status = 0;
+    vfs_resp.status   = 0;
     vfs_resp.data_len = 0;
 
     pisces_xbuf_complete(xbuf_desc, (u8 *)&vfs_resp, sizeof(struct pisces_lcall_resp));
@@ -92,12 +100,14 @@ int enclave_vfs_close_lcall(struct pisces_enclave * enclave,
 }
 
 
-int enclave_vfs_size_lcall(struct pisces_enclave * enclave, 
-			   struct pisces_xbuf_desc * xbuf_desc, 
-			   struct vfs_size_lcall * lcall) {
-    struct enclave_fs * fs_state = &(enclave->fs_state);
-    struct pisces_lcall_resp vfs_resp;
-    struct file * file_ptr = NULL;
+int 
+enclave_vfs_size_lcall(struct pisces_enclave   * enclave, 
+		       struct pisces_xbuf_desc * xbuf_desc, 
+		       struct vfs_size_lcall   * lcall) 
+{
+    struct enclave_fs        * fs_state = &(enclave->fs_state);
+    struct file              * file_ptr = NULL;
+    struct pisces_lcall_resp   vfs_resp;
 
     file_ptr = (struct file *)lcall->file_handle;
     
@@ -107,14 +117,14 @@ int enclave_vfs_size_lcall(struct pisces_enclave * enclave,
 	printk("File %p does not exist\n", file_ptr);
 
 	// File does not exist
-	vfs_resp.status = -1;
-	vfs_resp.data_len = 0;
+	vfs_resp.status   = -1;
+	vfs_resp.data_len =  0;
+
 	pisces_xbuf_complete(xbuf_desc, (u8 *)&vfs_resp, sizeof(struct pisces_lcall_resp));
 	return 0;
     }
 
-    vfs_resp.status = file_size(file_ptr);
-
+    vfs_resp.status   = file_size(file_ptr);
     vfs_resp.data_len = 0;
 
     pisces_xbuf_complete(xbuf_desc, (u8 *)&vfs_resp, sizeof(struct pisces_lcall_resp));
@@ -124,26 +134,33 @@ int enclave_vfs_size_lcall(struct pisces_enclave * enclave,
 
 
 
-int enclave_vfs_read_lcall(struct pisces_enclave * enclave, 
-			   struct pisces_xbuf_desc * xbuf_desc, 
-			   struct vfs_read_lcall * lcall) {
-    struct enclave_fs * fs_state = &(enclave->fs_state);
-    struct pisces_lcall_resp vfs_resp;
-    struct file * file_ptr = NULL;
-    u64 offset = lcall->offset;
-    u64 read_len = lcall->length;
+int 
+enclave_vfs_read_lcall(struct pisces_enclave   * enclave, 
+		       struct pisces_xbuf_desc * xbuf_desc, 
+		       struct vfs_read_lcall   * lcall)
+{
+    struct enclave_fs        * fs_state = &(enclave->fs_state);
+    struct file              * file_ptr = NULL;
+    struct pisces_lcall_resp   vfs_resp;
+
+    u64 offset           = lcall->offset;
+    u64 read_len         = lcall->length;
     u64 total_bytes_read = 0;
+
     u32 i = 0;
 
     file_ptr = (struct file *)lcall->file_handle;
+
     debug("FS: Reading file %p\n", file_ptr);
     
     if (!htable_search(fs_state->open_files, (uintptr_t)file_ptr)) {
 	// File does not exist
+
 	printk("File %p does not exist\n", file_ptr);
 
-	vfs_resp.status = -1;
-	vfs_resp.data_len = 0;
+	vfs_resp.status   = -1;
+	vfs_resp.data_len =  0;
+
 	pisces_xbuf_complete(xbuf_desc, (u8 *)&vfs_resp, sizeof(struct pisces_lcall_resp));
 
 	return 0;
@@ -151,8 +168,9 @@ int enclave_vfs_read_lcall(struct pisces_enclave * enclave,
 
     for (i = 0; i < lcall->num_descs; i++) {
 	struct vfs_buf_desc * desc = &(lcall->descs[i]);
-	u32 desc_bytes_read = 0;
-	u32 desc_bytes_left = desc->size;
+	u32 desc_bytes_read        = 0;
+	u32 desc_bytes_left        = desc->size;
+
 	u64 ret = 0;
 	
 	if (total_bytes_read >= read_len) {
@@ -169,9 +187,9 @@ int enclave_vfs_read_lcall(struct pisces_enclave * enclave,
 		break;
 	    }
 
-	    desc_bytes_read += ret;
-	    desc_bytes_left -= ret;
-	    offset += ret;
+	    desc_bytes_read  += ret;
+	    desc_bytes_left  -= ret;
+	    offset           += ret;
 	    total_bytes_read += ret;
 	}
 	
@@ -182,7 +200,7 @@ int enclave_vfs_read_lcall(struct pisces_enclave * enclave,
     }
 
     
-    vfs_resp.status = total_bytes_read;
+    vfs_resp.status   = total_bytes_read;
     vfs_resp.data_len = 0;
 
     pisces_xbuf_complete(xbuf_desc, (u8 *)&vfs_resp, sizeof(struct pisces_lcall_resp));
@@ -190,26 +208,32 @@ int enclave_vfs_read_lcall(struct pisces_enclave * enclave,
     return 0;
 }
 
-int enclave_vfs_write_lcall(struct pisces_enclave * enclave, 
-			    struct pisces_xbuf_desc * xbuf_desc, 
-			    struct vfs_write_lcall * lcall) {
-    struct enclave_fs * fs_state = &(enclave->fs_state);
-    struct pisces_lcall_resp vfs_resp;
-    struct file * file_ptr = NULL;
-    u64 offset = lcall->offset;
-    u64 write_len = lcall->length;
+int 
+enclave_vfs_write_lcall(struct pisces_enclave   * enclave, 
+			struct pisces_xbuf_desc * xbuf_desc, 
+			struct vfs_write_lcall  * lcall) 
+{
+    struct enclave_fs       * fs_state = &(enclave->fs_state);
+    struct file             * file_ptr = NULL;
+    struct pisces_lcall_resp  vfs_resp;
+
+    u64 offset              = lcall->offset;
+    u64 write_len           = lcall->length;
     u64 total_bytes_written = 0;
+
     u32 i = 0;
 
     file_ptr = (struct file *)lcall->file_handle;
+
     debug("writing file %p\n", file_ptr);    
 
     if (!htable_search(fs_state->open_files, (uintptr_t)file_ptr)) {
 	// File does not exist
 	printk("File %p does not exist\n", file_ptr);
 
-	vfs_resp.status = -1;
-	vfs_resp.data_len = 0;
+	vfs_resp.status   = -1;
+	vfs_resp.data_len =  0;
+
 	pisces_xbuf_complete(xbuf_desc, (u8 *)&vfs_resp, sizeof(struct pisces_lcall_resp));
 
 	return 0;
@@ -217,8 +241,8 @@ int enclave_vfs_write_lcall(struct pisces_enclave * enclave,
 
     for (i = 0; i < lcall->num_descs; i++) {
 	struct vfs_buf_desc * desc = &(lcall->descs[i]);
-	u32 desc_bytes_written = 0;
-	u32 desc_bytes_left = desc->size;
+	u32 desc_bytes_written     = 0;
+	u32 desc_bytes_left        = desc->size;
 	u64 ret = 0;
 	
 	if (total_bytes_written >= write_len) {
@@ -227,17 +251,17 @@ int enclave_vfs_write_lcall(struct pisces_enclave * enclave,
 
 	while (desc_bytes_left > 0) {
 	    ret = file_write(file_ptr, 
-			    __va(desc->phys_addr + desc_bytes_written), 
-			    desc_bytes_left, 
-			    offset);
+			     __va(desc->phys_addr + desc_bytes_written), 
+			     desc_bytes_left, 
+			     offset);
 
 	    if (ret == 0) {
 		break;
 	    }
 
-	    desc_bytes_written += ret;
-	    desc_bytes_left -= ret;
-	    offset += ret;
+	    desc_bytes_written  += ret;
+	    desc_bytes_left     -= ret;
+	    offset              += ret;
 	    total_bytes_written += ret;
 	}
 	
@@ -248,7 +272,7 @@ int enclave_vfs_write_lcall(struct pisces_enclave * enclave,
     }
 
     
-    vfs_resp.status = total_bytes_written;
+    vfs_resp.status   = total_bytes_written;
     vfs_resp.data_len = 0;
 
     pisces_xbuf_complete(xbuf_desc, (u8 *)&vfs_resp, sizeof(struct pisces_lcall_resp));
@@ -260,10 +284,11 @@ int enclave_vfs_write_lcall(struct pisces_enclave * enclave,
 
 
 
-int init_enclave_fs(struct pisces_enclave * enclave) {
+int 
+init_enclave_fs(struct pisces_enclave * enclave) 
+{
     struct enclave_fs * fs_state = &(enclave->fs_state);
-    
-    fs_state->open_files = create_htable(0, &file_hash_fn, &file_eq_fn);
+    fs_state->open_files         = create_htable(0, &file_hash_fn, &file_eq_fn);
 
     return 0;
 }
--- src/file_io.c
+++ src/file_io.c
@@ -21,22 +21,26 @@
 
 int file_mkdir(const char * pathname, unsigned short perms, int recurse);
 
-static int mkdir_recursive(const char * path, unsigned short perms) {
-    char * tmp_str = NULL;
-    char * dirname_ptr;
-    char * tmp_iter;
+static int 
+mkdir_recursive(const char    * path, 
+		unsigned short  perms)
+{
+    char * tmp_str     = NULL;
+    char * dirname_ptr = NULL;
+    char * tmp_iter    = NULL;
 
     tmp_str = kmalloc(strlen(path) + 1, GFP_KERNEL);
+
     if (!tmp_str) { 
 	printk(KERN_ERR "Cannot kmallocate in mkdir recursive\n");
 	return -1;
     }
 
-    memset(tmp_str, 0, strlen(path) + 1);
+    memset(tmp_str,  0,    strlen(path) + 1);
     strncpy(tmp_str, path, strlen(path));
 
     dirname_ptr = tmp_str;
-    tmp_iter = tmp_str;
+    tmp_iter    = tmp_str;
 
     // parse path string, call file_mkdir recursively.
 
@@ -47,7 +51,7 @@ static int mkdir_recursive(const char * path, unsigned short perms) {
 	while ((*tmp_iter != '/') && 
 	       (*tmp_iter != '\0')) {
 
-	    if ( (!isprint(*tmp_iter))) {
+	    if ( (!isprint(*tmp_iter)) ) {
 		printk(KERN_ERR "Invalid character in path name (%d)\n", *tmp_iter);
 		kfree(tmp_str);
 		return -1;
@@ -64,6 +68,7 @@ static int mkdir_recursive(const char * path, unsigned short perms) {
 
 	// Ignore empty directories
 	if ((tmp_iter - dirname_ptr) > 1) {
+
 	    if (file_mkdir(tmp_str, perms, 0) != 0) {
 		printk(KERN_ERR "Could not create directory (%s)\n", tmp_str);
 		kfree(tmp_str);
@@ -87,17 +92,21 @@ static int mkdir_recursive(const char * path, unsigned short perms) {
     return 0;
 }
 
-int file_mkdir(const char * pathname, unsigned short perms, int recurse) {
+int 
+file_mkdir(const char     * pathname, 
+	   unsigned short   perms,
+	   int              recurse) 
+{
     /* Welcome to the jungle... */
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,41)
     /* DO NOT REFERENCE THIS VARIABLE */
     /* It only exists to provide version compatibility */
-    struct path tmp_path; 
+    struct path     tmp_path; 
 #endif
 
-    struct path * path_ptr = NULL;
-    struct dentry * dentry;
+    struct path   * path_ptr = NULL;
+    struct dentry * dentry   = NULL;
     int ret = 0;
 
 
@@ -120,23 +129,23 @@ int file_mkdir(const char * pathname, unsigned short perms, int recurse) {
 	    return 0;
 	}
 
-	if (path_lookup(pathname, LOOKUP_PARENT | LOOKUP_FOLLOW, &nd) != 0) {
+	if (path_lookup(pathname, LOOKUP_PARENT    | LOOKUP_FOLLOW, &nd) != 0) {
 	    return -1;
 	}
 #endif
 
 	if (ret != 0) {
-	    printk(KERN_ERR "%s:%d - Error: kern_path_parent() returned error for (%s)\n", __FILE__, __LINE__, 
-		   pathname);
+	    printk(KERN_ERR "%s:%d - Error: kern_path_parent() returned error for (%s)\n", 
+		   __FILE__, __LINE__, pathname);
 	    return -1;
 	}
 	
-	dentry = lookup_create(&nd, 1);
+	dentry   = lookup_create(&nd, 1);
 	path_ptr = &(nd.path);
     }
 #else 
     {
-	dentry = kern_path_create(AT_FDCWD, pathname, &tmp_path, 1);
+	dentry   = kern_path_create(AT_FDCWD, pathname, &tmp_path, 1);
 	
 	if (IS_ERR(dentry)) {
 	    return 0;
@@ -158,10 +167,12 @@ int file_mkdir(const char * pathname, unsigned short perms, int recurse) {
 }
 
 
-struct file * file_open(const char * path, int mode) {
+struct file * 
+file_open(const char * path, 
+	  int          mode) 
+{
     struct file * file_ptr = NULL;	
     
-
     file_ptr = filp_open(path, mode | O_LARGEFILE, 0);
     
     if (IS_ERR(file_ptr)) {
@@ -172,14 +183,18 @@ struct file * file_open(const char * path, int mode) {
     return file_ptr;
 }
 
-int file_close(struct file * file_ptr) {
+int 
+file_close(struct file * file_ptr) 
+{
 
     filp_close(file_ptr, NULL);
     
     return 0;
 }
 
-unsigned long long file_size(struct file * file_ptr) {
+unsigned long long 
+file_size(struct file * file_ptr) 
+{
     struct kstat s;
     int ret;
     
@@ -197,15 +212,20 @@ unsigned long long file_size(struct file * file_ptr) {
     return s.size;
 }
 
-unsigned long long file_read(struct file * file_ptr, void * buffer, unsigned long long length, unsigned long long offset){
-    ssize_t ret;
+unsigned long long 
+file_read(struct file       * file_ptr, 
+	  void              * buffer, 
+	  unsigned long long  length, 
+	  unsigned long long  offset)
+{
+    ssize_t      ret;
     mm_segment_t old_fs;
 	
     old_fs = get_fs();
     set_fs(get_ds());
-	
-    ret = vfs_read(file_ptr, buffer, length, &offset);
-	
+
+    ret    = vfs_read(file_ptr, buffer, length, &offset);
+
     set_fs(old_fs);
 	
     if (ret <= 0) {
@@ -216,14 +236,19 @@ unsigned long long file_read(struct file * file_ptr, void * buffer, unsigned lon
 }
 
 
-unsigned long long file_write(struct file * file_ptr, void * buffer, unsigned long long length, unsigned long long offset) {
+unsigned long long 
+file_write(struct file        * file_ptr, 
+	   void               * buffer, 
+	   unsigned long long   length, 
+	   unsigned long long   offset) 
+{
     mm_segment_t old_fs;
-    ssize_t ret;
+    ssize_t      ret;
 
     old_fs = get_fs();
     set_fs(get_ds());
 
-    ret = vfs_write(file_ptr, buffer, length, &offset);
+    ret    = vfs_write(file_ptr, buffer, length, &offset);
 	
     set_fs(old_fs);
 
